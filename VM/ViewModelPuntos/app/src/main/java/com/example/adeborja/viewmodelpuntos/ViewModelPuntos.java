98140350e894951f082cfa45af8266ea63183ca9
Unknown <angeldaviddeborjagarcia@gmail.com>
2018-12-11 10:14:15 +0100

    11/12/2018
    Terminado el ejercicio con ViewModel. Hay que añadir un par de lineas en el proguard para que no pete.

--- VM/ViewModelPuntos/app/src/main/java/com/example/adeborja/viewmodelpuntos/ViewModelPuntos.java
+++ VM/ViewModelPuntos/app/src/main/java/com/example/adeborja/viewmodelpuntos/ViewModelPuntos.java
@@ -1,10 +1,22 @@
 package com.example.adeborja.viewmodelpuntos;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 public class ViewModelPuntos extends ViewModel {
 
     private int puntosA=0, puntosB=0;
+    private MutableLiveData<Integer> vecesPulsado;
+
+    public MutableLiveData<Integer> getVecesPulsado()
+    {
+        if(vecesPulsado==null)
+        {
+            vecesPulsado = new MutableLiveData<Integer>();
+            vecesPulsado.setValue(0);
+        }
+        return vecesPulsado;
+    }
 
     public int getPuntosA() {
         return puntosA;
--- VM/ViewModelPuntos/app/src/main/java/com/example/adeborja/viewmodelpuntos/VmPuntos.java
+++ VM/ViewModelPuntos/app/src/main/java/com/example/adeborja/viewmodelpuntos/VmPuntos.java
@@ -1,6 +1,8 @@
 package com.example.adeborja.viewmodelpuntos;
 
-import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +12,11 @@ import android.widget.TextView;
 public class VmPuntos extends AppCompatActivity {
 
     private ViewModelPuntos mViewModel;
-    TextView puntosA, puntosB;
-    Button btnSumaA, btnRestaA, btnSumaB, btnRestaB;
+    private TextView puntosA, puntosB, veces;
+    private Button btnSumaA, btnRestaA, btnSumaB, btnRestaB;
+
+    private int auxInt;
+    private String auxString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +25,38 @@ public class VmPuntos extends AppCompatActivity {
 
         puntosA = (TextView)findViewById(R.id.txvPuntosA);
         puntosB = (TextView)findViewById(R.id.txvPuntosB);
+        veces = (TextView)findViewById(R.id.txvVeces);
         btnSumaA = (Button)findViewById(R.id.btnSumarA);
         btnSumaB = (Button)findViewById(R.id.btnSumarB);
         btnRestaA = (Button)findViewById(R.id.btnRestarA);
         btnRestaB = (Button)findViewById(R.id.btnRestarB);
 
-        mViewModel = ViewModelProvider.of(this).get(ViewModelPuntos.class);
+        mViewModel = ViewModelProviders.of(this).get(ViewModelPuntos.class);
 
-        puntosA.setText(mViewModel.getPuntosA());
-        puntosB.setText(mViewModel.getPuntosB());
+        //Crear el observador para el viewModel
+        final Observer<Integer> observadorVeces = new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer s) {
+                veces.setText(String.valueOf(s));
+            }
+        };
+
+        //Observar el LiveData
+        mViewModel.getVecesPulsado().observe(this, observadorVeces);
+
+
+        //mViewModel.setPuntosA(0);
+        //mViewModel.setPuntosB(0);
+
+        puntosA.setText(String.valueOf(mViewModel.getPuntosA()));
+        puntosB.setText(String.valueOf(mViewModel.getPuntosB()));
 
         btnSumaA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mViewModel.setPuntosA(mViewModel.getPuntosA()+1);
+                modificarPuntosA();
+                actualizarVeces();
             }
         });
 
@@ -41,6 +64,8 @@ public class VmPuntos extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mViewModel.setPuntosB(mViewModel.getPuntosB()+1);
+                modificarPuntosB();
+                actualizarVeces();
             }
         });
 
@@ -48,6 +73,8 @@ public class VmPuntos extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mViewModel.setPuntosA(mViewModel.getPuntosA()-1);
+                modificarPuntosA();
+                actualizarVeces();
             }
         });
 
@@ -55,10 +82,30 @@ public class VmPuntos extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mViewModel.setPuntosB(mViewModel.getPuntosB()-1);
+                modificarPuntosB();
+                actualizarVeces();
             }
         });
 
+        //modificarPuntosA();
+        //modificarPuntosA();
 
+    }
+
+    public void actualizarVeces()
+    {
+        auxInt = mViewModel.getVecesPulsado().getValue()+1;
+        mViewModel.getVecesPulsado().setValue(auxInt);
+    }
+
+    public void modificarPuntosA()
+    {
+        puntosA.setText(String.valueOf(mViewModel.getPuntosA()));
+    }
+
+    public void modificarPuntosB()
+    {
+        puntosB.setText(String.valueOf(mViewModel.getPuntosB()));
     }
 
 
